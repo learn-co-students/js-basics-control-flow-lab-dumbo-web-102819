29c4e20ae721d49f7d39268d0f3d68ba0553dc3c
ozubergz <jmin499@gmail.com>
2019-12-09 12:49:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(num){
+  if(num > 2500) {
+    return "No can do."
+  } else if(num > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  } else if(num <= 400) {
+    return 'This one is on me!'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === "NYC" ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(gratitude){
+  switch(gratitude) {
+    case "generous":
+      return 'Thank you so much.';
+    case "not as generous":
+      return 'Thank you.';
+    case 'thanks for everything':
+      return "Bye."
+  }
 }
